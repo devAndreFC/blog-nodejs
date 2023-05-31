4d647aeaa1554bd0316ddcec5ff6b653054faf0a
Andre Oliveira <dev.andrefc@gmail.com>
2023-05-31 09:26:26 -0300

    Finalização das rotas e viwes do CRUD de categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -4,10 +4,45 @@ const Category = require('./Category');
 const slugify = require('slugify');
 // const flash = require('connect-flash');
 
+// Ver categories
 router.get("/admin/categories/newCategory", (req, res) => {
     res.render("admin/categories/newCategory")
 })
 
+// Update categoria
+router.post("/categories/updateCategory", (req, res) => {
+    var id = req.body.id
+    var title = req.body.title
+
+    Category.update(
+        {title: title, slug: slugify(title)}, {
+        where:{
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/categories")
+    })
+})
+
+// Página edição Category
+router.get("/admin/categories/editCategory/:id", (req, res) => {
+    var id = req.params.id;
+    if (isNaN(id)) {
+        res.redirect("/admin/categories")
+    }
+    
+    Category.findByPk(id).then(category => {
+        if (category != undefined) {
+            res.render("admin/categories/editCategory", {category: category});
+        }else{
+            res.redirect("/admin/categories")
+        }
+    }).catch(erro => {
+        res.redirect("/admin/categories")
+    })
+})
+
+// Deletar category
 router.post("/categories/delete", (req, res) => {
     var id = req.body.id
     if (id !== undefined) {
@@ -26,6 +61,8 @@ router.post("/categories/delete", (req, res) => {
         res.redirect("/admin/categories")
     }
 })
+
+// Criar category
 router.post("/categories/save", (req, res) => {
     var title = req.body.title // atributo 'name' do campo que desejo pegar
     if (title != undefined) {
@@ -34,7 +71,7 @@ router.post("/categories/save", (req, res) => {
             slug: slugify(title)
         }).then(() => {
             // req.flash('success', 'Categoria cadastrada com sucesso!');
-            res.redirect("/");
+            res.redirect("/admin/categories");
         })
     }else{
         res.redirect("/admin/categories/newCategory")
@@ -43,7 +80,7 @@ router.post("/categories/save", (req, res) => {
 
 router.get("/admin/categories", (req, res) => {
     Category.findAll().then(categories => {
-        res.render("admin/categories/indexCategories", {categories: categories})
+        res.render("admin/categories/indexCategory", {categories: categories})
     })
     
 })
