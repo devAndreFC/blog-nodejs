c4307b9fb06683f3d17e2c9a80e45c1cc93a31e1
Andre Oliveira <dev.andrefc@gmail.com>
2023-06-01 15:48:23 -0300

    Criação da página de exibição dos artigos por categoria

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ app.get("/", (req, res) => {
     })
 })
 
+// menu navbar das categorias
 app.get("/:slug", (req, res) => {
     var slug = req.params.slug
     Article.findOne({
@@ -53,6 +54,27 @@ app.get("/:slug", (req, res) => {
         if(article !== undefined){
             Category.findAll().then(categories => {
                 res.render("article", {article: article, categories: categories });  
+            })
+        }else{
+            res.redirect("/")
+        }
+    }).catch(err => {
+        res.redirect("/")
+    })
+})
+
+// rota das categorias, clicando no link da navbar
+app.get("/category/:slug", (req, res) => {
+    var slug = req.params.slug
+    Category.findOne({
+        where:{
+            slug: slug
+        },
+        include:[{model: Article}]
+    }).then(category => {
+        if (category !== undefined) {
+            Category.findAll().then(categories => {
+            res.render("index", {articles: category.articles, categories: categories})
             })
         }else{
             res.redirect("/")
