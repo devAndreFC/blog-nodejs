635f1b2fb29d80c0ed04e1bf731716c17aa7836c
Andre Oliveira <dev.andrefc@gmail.com>
2023-06-01 09:02:05 -0300

    Criação do CRUD dos artigos, utilizando a biblioteca Tinymce para edição textual

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,13 +1,57 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('../categories/Category')
+const Article = require('./Article')
+const slugify = require('slugify')
 
-router.get("/articles", (req, res) => {
-    res.send('Rota de artigos')
+router.get("/admin/articles", (req, res) => {
+    Article.findAll({
+        include: [{model: Category}]
+    }).then(articles => {
+        res.render("admin/articles/indexArticle", {articles: articles})
+    })
 })
 
-router.get("/admin/articles/new", (req, res) => {
-    res.send('Rota de criação de artigos')
+router.get("/admin/articles/newArticle", (req, res) => {
+    Category.findAll().then(categories => {
+        res.render("admin/articles/newArticle", {categories: categories})
+    })
 })
 
+
+// create a new article
+router.post("/articles/saveArticle", (req, res) => {
+    var title = req.body.title
+    var body = req.body.body
+    var category = req.body.category
+
+    Article.create({
+        title: title,
+        slug: slugify(title), 
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect("/admin/articles")
+    })
+})
+
+router.post('/articles/delete', (req, res) => {
+    var id = req.body.id
+    if (id != undefined) {
+        if(!isNaN(id)) {
+            Article.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/articles")
+            })
+        }else{
+            res.redirect("/admin/articles")
+        }
+    }else{
+        res.redirect("/admin/articles")
+    }
+})
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -1,7 +1,8 @@
 const Sequelize = require("sequelize");
 const connection = new Sequelize('bdblog', 'root', 'root1415', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: "-03:00"
 });
 
 module.exports = connection;
