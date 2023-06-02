2c2c8ba5cedffa7f813d426189310fa6587bbf9f
Andre Oliveira <dev.andrefc@gmail.com>
2023-06-02 16:38:11 -0300

    Criação da página de edição de artigo e update no BD

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -4,6 +4,46 @@ const Category = require('../categories/Category')
 const Article = require('./Article')
 const slugify = require('slugify')
 
+// Update artigo
+router.post("/articles/updateArticle", (req, res) => {
+    var id = req.body.id
+    var title = req.body.title
+    var body = req.body.body
+    var category = req.body.category
+
+    Article.update(
+        {title: title, slug: slugify(title), body: body, categoryId: category}, {
+        where:{
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/articles")
+    }).catch(err => {
+        res.redirect("/")
+    })
+})
+
+// Página edição artigo
+router.get("/admin/articles/editArticle/:id", (req, res) => {
+    var id = req.params.id;
+    if (isNaN(id)) {
+        res.redirect("/admin/articles")
+    }
+    Article.findByPk(id).then(article => {
+        if (article != undefined) {
+            Category.findAll().then(categories =>{
+                res.render("admin/articles/editArticle", {article: article, categories: categories});
+            })
+            
+        }else{
+            res.redirect("/admin/articles")
+        }
+    }).catch(erro => {
+        res.redirect("/admin/articles")
+    })
+})
+
+
 router.get("/admin/articles", (req, res) => {
     Article.findAll({
         include: [{model: Category}]
